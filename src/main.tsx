bc7703829e157c8570e4d819148f89ca10a46bcb
rinel <rineliniguezsosa@gmail.com>
2023-09-07 18:55:53 -0600

    feat: envolvemos el componente App por el componente Provider para propagar entre los componentes el store

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )

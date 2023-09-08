9b632be7a9ce44d6180e20de268f301c57885392
rinel <rineliniguezsosa@gmail.com>
2023-09-08 14:09:04 -0600

    feat: importando el store y estableciendo la prop store al componente provider
    - establecemos el store al componente App para propagar el state de la información hacia los componentes

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.tsx'
 import './index.css'
+import { store } from './store/store/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,

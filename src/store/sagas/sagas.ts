394da49206ff8f0e1c1a730ae680138f9e8a8344
rinel <rineliniguezsosa@gmail.com>
2023-09-08 18:04:45 -0600

    fix: eliminamos todo lo relacionado para la creación del middleware en el archivo sagas.ts

--- src/store/sagas/sagas.ts
+++ src/store/sagas/sagas.ts
@@ -1,3 +1,1 @@
-import createSagaMiddleware from 'redux-saga'  // esta importación es importante para la ejecución de tareas asincronas 
-
-export const sagaMiddleware = createSagaMiddleware()
+//funciones asincronas

04762a18b70aa5ebdd4af5f9e487d0e350e0e211
rinel <rineliniguezsosa@gmail.com>
2023-09-08 18:00:38 -0600

    fix: importando createSagaMiddleware de redux-saga en el archivo store.ts

--- src/store/store/store.ts
+++ src/store/store/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { sagaMiddleware } from '../sagas/sagas'
-
+import createSagaMiddleware from 'redux-saga'  // esta importación es importante para la ejecución de tareas asincronas 
 
 export const store = configureStore()
+

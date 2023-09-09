dec0320871518fee1659fb48a47ec8523b002c0c
rinel <rineliniguezsosa@gmail.com>
2023-09-08 22:55:59 -0600

    feat: importando rootReducer para establecerlo en el store de la aplicación
    - estableciendo la propiedad reducer con el valor del rootReducer para definir nuestros reducers

--- src/store/store/store.ts
+++ src/store/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import createSagaMiddleware from 'redux-saga'  // esta importación es importante para la ejecución de tareas asincronas 
+import { rootReducer } from '../reducers/reducer'
+
 const sagaMiddleware = createSagaMiddleware()
 
 export const store = configureStore({
-    middleware:sagaMiddleware
+    reducer:rootReducer,
 })
 

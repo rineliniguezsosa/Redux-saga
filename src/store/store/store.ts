e16a917ab746b4e84cf30578671bc71e8de7377d
rinel <rineliniguezsosa@gmail.com>
2023-09-08 18:09:22 -0600

    feat: establecemos el middleware en el store provisionalmente
    - establecemos el middleware:sagaMiddleware esto permitiria la ejecución de eventos secundarios y peticones asincronas
    - por el momento no hemos definido solicitudes http

--- src/store/store/store.ts
+++ src/store/store/store.ts
@@ -2,5 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import createSagaMiddleware from 'redux-saga'  // esta importación es importante para la ejecución de tareas asincronas 
 const sagaMiddleware = createSagaMiddleware()
 
-export const store = configureStore()
+export const store = configureStore({
+    middleware:sagaMiddleware
+})
 

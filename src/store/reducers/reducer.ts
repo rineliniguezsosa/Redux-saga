76f7860ee0d6a49019f026291da991b01fed79c3
rinel <rineliniguezsosa@gmail.com>
2023-09-09 19:27:26 -0600

    fix: eliminando la importación de combineReducers y la definición de los reducers

--- src/store/reducers/reducer.ts
+++ src/store/reducers/reducer.ts
@@ -1,10 +1,7 @@
-import { combineReducers } from 'redux'
+
 
 export const CardItem = (data = [],action:string) =>{
     console.log(data)
     console.log(action)
 }
 
-export const rootReducer = combineReducers({
-    Todo:CardItem
-})

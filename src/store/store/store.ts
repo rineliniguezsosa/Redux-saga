657983ed31da1fc9b6b2a5386169a2718542d6c9
rinel <rineliniguezsosa@gmail.com>
2023-09-09 19:29:33 -0600

    fix: importando el combineReducers de reduxjs/toolkit
    - eliminando combineReducers de redux

--- src/store/store/store.ts
+++ src/store/store/store.ts
@@ -1,12 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore,combineReducers } from '@reduxjs/toolkit'
 import createSagaMiddleware from 'redux-saga'  // esta importación es importante para la ejecución de tareas asincronas 
 import { rootReducer } from '../reducers/reducer'
-import { combineReducers } from 'redux'
+
 
 const sagaMiddleware = createSagaMiddleware()
 
 const reducer = combineReducers({
-    
+
 })
 
 export const store = configureStore({

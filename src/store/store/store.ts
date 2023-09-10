40a94ae3dd50bcf965b6898c44b0ae5684ba7bb0
rinel <rineliniguezsosa@gmail.com>
2023-09-09 19:23:24 -0600

    feat: importando combineReducers de redux en el archivo store.ts

--- src/store/store/store.ts
+++ src/store/store/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import createSagaMiddleware from 'redux-saga'  // esta importación es importante para la ejecución de tareas asincronas 
 import { rootReducer } from '../reducers/reducer'
+import { combineReducers } from 'redux'
 
 const sagaMiddleware = createSagaMiddleware()
 

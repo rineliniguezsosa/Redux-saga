9b58ef429d86d527ae825df73f793768db8140ad
rinel <rineliniguezsosa@gmail.com>
2023-09-12 16:54:49 -0600

    feat: importando la función generadora watchincrement en el archivo store.ts

--- src/store/store/store.ts
+++ src/store/store/store.ts
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import createSagaMiddleware from 'redux-saga'  // esta importación es importante para la ejecución de tareas asincronas 
 import { CountReducer } from '../slice/slice'
+import { watchincrement } from '../sagas'
 
 
 const sagaMiddleware = createSagaMiddleware()

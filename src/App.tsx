994e8de1cf9e585e1b9362139ed94dedd7bfa391
rinel <rineliniguezsosa@gmail.com>
2023-09-12 17:35:31 -0600

    feat: importando los métodos increment y decrementby
    - estos métodos serviran para la ejecución de las acciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react-refresh/only-export-components */
 import { Component } from 'react'
 import { connect } from 'react-redux'
+import { decrementby,increment } from './store'; //métodos del reducer ó slice
+
 
 class App extends Component{
   render(){

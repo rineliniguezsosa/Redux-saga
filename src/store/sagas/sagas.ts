6c9747a97725f2332f6ecd13ac05f322542b28a1
rinel <rineliniguezsosa@gmail.com>
2023-09-12 16:37:52 -0600

    feat: creando la función generadora de saga watchincrement
    - esta función generadora despachara la acción counter/incrementSaga
    -utilizamos la función takeEvery para despachar la función generadora

--- src/store/sagas/sagas.ts
+++ src/store/sagas/sagas.ts
@@ -5,3 +5,7 @@ import { increment } from '../slice/slice'
 function* incrementSaga(){
     yield put(increment());
 }
+
+export function* watchincrement(){
+    yield takeEvery('counter/incrementSaga',incrementSaga);
+}

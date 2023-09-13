d6bf9acd1421cfb82806a19142a991e3c50b4291
rinel <rineliniguezsosa@gmail.com>
2023-09-13 17:56:15 -0600

    feat: estableciendo comentarios de las funciones de saga effects

--- src/store/sagas/catSaga.ts
+++ src/store/sagas/catSaga.ts
@@ -1,1 +1,5 @@
 import { put,takeEvery,call } from 'redux-saga/effects'
+
+//call allow call urls and apis
+//put allow call our actions 
+//takevery allow watch functions or watch a function when is call

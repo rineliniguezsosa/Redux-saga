60f8e5a6faf87784c3dce1005a520b5e97d3e36c
rinel <rineliniguezsosa@gmail.com>
2023-09-13 23:00:57 -0600

    feat: estableciendo la nueva propiedad getcatsfeth para añadir en el type Counterprops
    - la prop getcatsfetch es una función de retorno vacio

--- src/store/types/Datatypes.ts
+++ src/store/types/Datatypes.ts
@@ -1,7 +1,8 @@
 export type Counterprops = {
     value:number,
     increment:()=> void,
-    decrementby:(num:number)=>void
+    decrementby:(num:number)=>void,
+    getcatsfetch:()=>void
 }
 
 export type Countervalue = Omit<Counterprops,"increment"|"decrementby">

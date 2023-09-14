3a69673eb5a2818a4a21a8f23029e4ddb0ba32a1
rinel <rineliniguezsosa@gmail.com>
2023-09-13 21:15:57 -0600

    feat: creando la función generadora getCats
    - esta función extraera los datos de la api

--- src/store/sagas/catSaga.ts
+++ src/store/sagas/catSaga.ts
@@ -3,3 +3,7 @@ import { put,takeEvery,call } from 'redux-saga/effects'
 //call allow call urls and apis
 //put allow call our actions 
 //takevery allow watch functions or watch a function when is call
+
+function* getCats(){
+    
+}
